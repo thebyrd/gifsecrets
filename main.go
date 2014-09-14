9aa575ad44584d36a6ea8e3cc6699bb85e5491c6
David Byrd <byrd@bowery.io>
2014-09-14 16:19:04 -0400

    init

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"code.google.com/p/go-uuid/uuid"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"regexp"
+)
+
+// Encode stores a uuid in the comment section of a gif
+func Encode(path, id string) error {
+	if id == "" {
+		id = uuid.New()
+	}
+
+	cmd := exec.Command("/usr/local/bin/gifsicle", path, "--no-comments", "-c", "\""+id+"\"", "#0", "#1-")
+
+	outfile, err := os.Create("./out.gif")
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	writer := bufio.NewWriter(outfile)
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	go io.Copy(writer, stdoutPipe)
+	cmd.Wait()
+	return nil
+}
+
+// Decode returns the secret encoded in the gif comment extension
+func Decode(path string) (string, error) {
+	cmd := exec.Command("/usr/local/bin/gifsicle", "-I", path, "#0")
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	reg := regexp.MustCompile("\"(.*?)\"") // this captures everything in between quotes which is pretty ghetto
+	return string(reg.Find(out)), nil
+}
---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+const TestSecret = "test secret"
+
+func TestGifSecrets(t *testing.T) {
+	if err := Encode("coin.gif", TestSecret); err != nil {
+		t.Fatal(err)
+	}
+
+	secret, err := Decode("out.gif")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// intentionally weird behavior until I learn regex
+	if secret != "\""+TestSecret+"\"" {
+		t.Errorf("Decoded secret was %s instead of %s", secret, TestSecret)
+	}
+}
